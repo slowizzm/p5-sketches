2ed12687de4ad219ab957577d44d58c199399cee
Raymond G McCord <gene33@live.missouristate.edu>
2016-07-27 16:38:16 -0500

    Add files via upload

---
+++ moireMoire/sketch.js
@@ -0,0 +1,44 @@
+/* Raymond G McCord (slow_izzm)
+   06/03/2016
+   
+   This work is licensed under a Creative Commons 3.0 License.
+    (Attribution - NonCommerical - ShareAlike)
+ 		http://creativecommons.org/licenses/by-nc-sa/3.0/
+*/
+
+
+var x,
+    y,
+    i = 0;
+
+function setup() {
+  createCanvas(600,400);
+  x = width;
+  y = height;
+  
+  noCursor();
+}
+
+function draw() {
+  background(255);
+
+  for (var pat = 0; pat < 1600; pat += 6) {
+    stroke(0);
+    noFill();
+    ellipse(width / 2, height / 2, mouseX - pat - i, mouseY - pat + i);
+    // line(width / 2, height / 2, pat + i, height/2 - pat + i);
+
+
+  }
+
+  i += 1;
+  if (i == width * 1.5) {
+    i = -i;
+
+  }
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
---
+++ symmButterfly/sketch.js
@@ -0,0 +1,79 @@
+/* Raymond G McCord (slow_izzm)
+   06/22/2016
+   
+   This work is licensed under a Creative Commons 3.0 License.
+    (Attribution - NonCommerical - ShareAlike)
+ 		http://creativecommons.org/licenses/by-nc-sa/3.0/
+*/
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noStroke();
+ fill(255); 
+}
+
+function draw() {
+  background(33);
+  push();
+  translate(width/2, height/2);
+  for (var angle = -90; angle < 90; angle += 0.5) {
+    for (var i = 1; i < 2; i += 0.2) {
+       var a = i*100;
+       var t = angle+frameCount+1.5+a;
+       var x = 16*pow(cos(radians(t)), 0.3);
+       var y = -13*cos(radians(t)) + 5*cos(radians(0.3*t)) * 2*cos(radians(3*t)) + cos(radians(4*t));
+       var s = cos(radians(angle))*i*3;
+       ellipse(x*i*8, y*i*8, s, s); 
+    }
+  }  
+  pop();
+  
+  push();
+  translate(width/2, height/2);
+  for (var angle = -90; angle < 90; angle += 0.5) {
+    for (var i = 1; i < 2; i += 0.2) {
+       var a = i*100;
+       var t = angle+frameCount+1.5+a;
+       var x = -16*pow(cos(radians(t)), 0.3);
+       var y = -13*cos(radians(t)) + 5*cos(radians(0.3*t)) * 2*cos(radians(3*t)) + cos(radians(4*t));
+       var s = cos(radians(angle))*i*3;
+       ellipse(x*i*8, y*i*8, s, s); 
+    }
+  }  
+  pop();
+  
+  push();
+  translate(width/2, height/2);
+  for (var angle = -90; angle < 90; angle += 0.5) {
+    for (var i = 1; i < 2; i += 0.2) {
+       var a = i*100;
+       var t = angle+frameCount+1.5+a;
+       var x = -16*pow(cos(radians(t)), 0.3);
+       var y = 13*cos(radians(t)) + 5*cos(radians(0.3*t)) * 2*cos(radians(3*t)) + cos(radians(4*t));
+       var s = cos(radians(angle))*i*3;
+       ellipse(x*i*8, y*i*8, s, s); 
+    }
+  }  
+  pop();
+  
+  push();
+  translate(width/2, height/2);
+  for (var angle = -90; angle < 90; angle += 0.5) {
+    for (var i = 1; i < 2; i += 0.2) {
+       var a = i*100;
+       var t = angle+frameCount+1.5+a;
+       var x = 16*pow(cos(radians(t)), 0.3);
+       var y = 13*cos(radians(t)) + 5*cos(radians(0.3*t)) * 2*cos(radians(3*t)) + cos(radians(4*t));
+       var s = cos(radians(angle))*i*3;
+       ellipse(x*i*8, y*i*8, s, s); 
+    }
+  }  
+  pop();
+  
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
