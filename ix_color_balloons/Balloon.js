891d0429d18deaa0694460e5d144109cee9d2e0f
Raymond G McCord <raymond@selfinteractive.com>
2019-04-22 11:37:03 -0500

    update: convert to class

--- ix_color_balloons/Balloon.js
+++ ix_color_balloons/Balloon.js
@@ -1,91 +1,110 @@
-function Balloon(_x, _y, _size, _c) {
+class Balloon {
+  constructor(x, y, size, c) {
 
-	this.pos = createVector(_x, _y);
-	this.vel = createVector(0, -2);
-	this.acc = createVector(0, 0);
-	this.size = _size;
-	this.maxspeed = 33;
-	this.maxforce = 0.01;
-	this.c = _c;
+    this.pos = createVector(x, y);
+    this.vel = createVector(0, -2);
+    this.acc = createVector(0, 0);
+    this.size = size;
+    this.maxspeed = 33;
+    this.maxforce = 0.01;
+    this.c = c;
+  }
 
-	this.update = function(_v) {
-		this.vel.add(this.acc);
-		this.pos.add(this.vel);
-		this.acc.mult(0);
-		this.vel.mult(_v);
-	}
+  update(v) {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+    this.vel.mult(v);
+    
+    return this;
+  }
 
-	this.seek = function(target) {
-		let desired = p5.Vector.sub(target, this.pos);
-		desired.setMag(this.maxspeed);
-		let steer = p5.Vector.sub(desired, this.vel);
-		steer.limit(this.maxforce);
+  seek(targ) {
+    let desired = p5.Vector.sub(targ, this.pos);
+    desired.setMag(this.maxspeed);
+    let steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxforce);
 
-		if (desired <= this.size) {
-			this.vel.mult(0);
-		} else {
-			this.vel.mult(1);
-		}
+    if (desired <= this.size) {
+      this.vel.mult(0);
+    } else {
+      this.vel.mult(1);
+    }
 
-		this.applyForce(steer);
-	}
+    this.applyForce(steer);
 
-	this.applyForce = function(force) {
-		this.acc.add(force);
-	}
+    return this;
+  }
 
-	this.arrive = function(target) {
-		let desired = p5.Vector.sub(target, this.pos);
-		let d = desired.mag();
+  applyForce(f) {
+    this.acc.add(f);
+  }
 
-		if (d < height) {
-			let m = map(d, 0, height, 0, this.maxspeed);
-			desired.setMag(m);
-		} else {
-			desired.setMag(this.maxforce);
-		}
+  arrive(targ) {
+    let desired = p5.Vector.sub(targ, this.pos);
+    let d = desired.mag();
 
-		let steer = p5.Vector.sub(desired, this.vel);
-		steer.limit(this.maxspeed);
+    if (d < height) {
+      let m = map(d, 0, height, 0, this.maxspeed);
+      desired.setMag(m);
+    } else {
+      desired.setMag(this.maxforce);
+    }
 
-		if (desired <= this.size) {
-			this.vel.mult(0);
-		} else {
-			this.vel.mult(1);
-		}
+    let steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.maxspeed);
 
-		this.applyForce(steer);
-	}
+    if (desired <= this.size) {
+      this.vel.mult(0);
+    } else {
+      this.vel.mult(1);
+    }
 
-	this.display = function(_l) {
-		ellipse(CENTER);
-		fill(cols[this.c]);
-		ellipse(this.pos.x + 5, this.pos.y, this.size);
-		push();
-		fill(255);
-		textSize(42);
-		textAlign(CENTER);
-		textStyle(BOLD);
-		text(circLetters[_l], this.pos.x + 5, this.pos.y + 13);
-		pop();
-		
-		push();
-		beginShape();
-		vertex(this.pos.x+7,this.pos.y+36);
-		vertex(this.pos.x+9,this.pos.y+39);
-		vertex(this.pos.x+11,this.pos.y+42);
-		vertex(this.pos.x+7,this.pos.y+42);
-		endShape(CLOSE);
-		pop();
-	}
+    this.applyForce(steer);
 
-	this.edges = function() {
-		if (this.pos.x <= 0 || this.pos.x >= width) {
-			this.vel.x *= -1;
-		}
-		if (this.pos.y <= 0 || this.pos.y >= height) {
-			this.pos.y = mouseY;
-			this.vel.y *= -1;
-		}
-	}
+    return this;
+  }
+
+  balloon(l) {
+    fill(cols[this.c]);
+    ellipse(this.pos.x + 5, this.pos.y, this.size);
+    push();
+    fill(255);
+    textSize(42);
+    textAlign(CENTER);
+    textStyle(BOLD);
+    text(circLetters[l], this.pos.x + 5, this.pos.y + 13);
+    pop();
+
+    push();
+    beginShape();
+    vertex(this.pos.x + 7, this.pos.y + 36);
+    vertex(this.pos.x + 9, this.pos.y + 39);
+    vertex(this.pos.x + 11, this.pos.y + 42);
+    vertex(this.pos.x + 7, this.pos.y + 42);
+    endShape(CLOSE);
+    pop();
+
+    return this;
+  }
+
+  edges() {
+    if (this.pos.x <= 0 || this.pos.x >= width) {
+      this.vel.x *= -1;
+    }
+    if (this.pos.y <= 0 || this.pos.y >= height) {
+      this.pos.y = mouseY;
+      this.vel.y *= -1;
+    }
+
+    return this;
+  }
+  
+  display(l) {
+      return this.balloon(l).edges();
+    }
+  
+  render(v,targ) {
+    return this.update(v).seek(targ).arrive(targ);
+  }
 }
