2b2ba8f5c7aca29681d7c91a5b05b3e39e6e6760
Raymond G McCord <raymondgmccord@gmail.com>
2019-11-27 12:14:27 -0600

    make ui responsive

--- InteractiveBeginShape/js/helpers.js
+++ InteractiveBeginShape/js/helpers.js
@@ -10,3 +10,13 @@ function closeShape() {
 function fillShape() {
   ib.isFill = !ib.isFill;
 }
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight - 120);
+  btn_printConsole.position(10, height + 10);
+  btn_clearConsole.position(150, height + 15);
+  swapType.position(280, height + 15);
+  btn_clearShape.position(20, height + 70);
+  chkbx_clear.position(150, height + 70);
+  chkbx_fill.position(260, height + 70);
+}
